5e56210c76647733bb0db49fbaa6fa2f5c8e858d
shashank <shash@mit.edu>
2021-11-14 23:32:12 +0000

    testing automation 3

--- compile.sh
+++ compile.sh
@@ -6,9 +6,21 @@ git commit -m "$*"
 
 ./bin/deploy --user
 
+sleep 1
+
 git add .
 
+sleep 2
+
 git commit -m "$*"
+
+sleep 2
+
+git push --set-upstream origin master --force
+
+sleep 1
+
+git push origin --all
 
 git checkout source
 
